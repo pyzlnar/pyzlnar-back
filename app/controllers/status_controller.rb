d2eb6ad7513a7f47962a67578985eab8139b15c4
pyzlnar@Monochrome <pyzlnar@gmail.com>
2017-02-02 16:32:37 -0600

    :wrench: Added basic rubocop
    Added basic rubocop, fixed the offenses.

--- app/controllers/status_controller.rb
+++ app/controllers/status_controller.rb
@@ -1,4 +1,6 @@
+# This controller is just used as a quick way to know if the api is live or not.
 class StatusController < ApplicationController
+  # GET /api/status
   def index
     head :no_content
   end
